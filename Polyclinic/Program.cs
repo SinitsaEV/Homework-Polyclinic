d3f0ba7d62fa6c3512ce81f087b080d658e3b7bb
Sinitsa <sinitsammi@gmail.com>
2024-12-02 15:15:48 +0300

    Polyclinic

---
+++ Polyclinic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polyclinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            int numberOfPatients;
+            int timeOfReceptionInMinutes = 10;
+            int hourInQueue;
+            int minuteInQueue;
+            int hourInMinutes = 60;
+
+            Console.Write(" Введите количество пациентов в очереди: ");
+            numberOfPatients = Convert.ToInt32(Console.ReadLine());
+
+            hourInQueue = timeOfReceptionInMinutes * numberOfPatients / hourInMinutes;
+            minuteInQueue = timeOfReceptionInMinutes * numberOfPatients % hourInMinutes;
+
+            Console.WriteLine($" Вы должны отстоять в очереди {hourInQueue} час(а) и {minuteInQueue} минут. ");
+        }
+    }
+}
